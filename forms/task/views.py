52e65a03f3d350d18d1690dd87c9a708a551dc60
Ruan <venancioruan11@gmail.com>
2023-11-09 16:47:57 -0300

    implementação da parte do banco de dados

--- forms/task/views.py
+++ forms/task/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from models import Pessoa
 # Create your views here.
 
 tasks = [1,2,3,4]
@@ -8,9 +8,13 @@ def index(request):
 
     name = request.POST.get('name')
 
+    pessoa = Pessoa(name=name)
+    pessoa.save()
+
+    pessoas = Pessoa.objects.all()
+
     return render(request, 'task/index.html', {
         "tasks":tasks,
         "name":name
-
 
     })
